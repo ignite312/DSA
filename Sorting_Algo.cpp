f6d084bfbfe0adc0bcca9a02d0f9308ab097a355
Emon <schrodingersrats@gmail.com>
2023-01-21 23:46:50 +0600

    Update Sorting_Algo.cpp

--- Sorting_Algo.cpp
+++ Sorting_Algo.cpp
@@ -62,16 +62,31 @@ void mergeSort(int l, int r) {
     mergeSort(mid+1, r);
     merge(l, r, mid);
 }
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1); 
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
 int main() {
 	int n;
     scanf("%d", &n);
     int arr[n];
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);  
-    }
+    for(int i = 0; i < n; i++)scanf("%d", &a[i]); 
 	mergeSort(0, n-1);
-	for(int i = 0; i < n; i++) {
-		printf("%d ", a[i]);
-	}
+	for(int i = 0; i < n; i++)printf("%d ", a[i]);
     return 0;
 }
