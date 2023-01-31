ff78e88f5bbf83a7b85029e5d45de970c322f4c4
Emon <schrodingersrats@gmail.com>
2023-01-31 19:58:09 +0600

    Update Sorting_Algo.cpp

--- Sorting_Algo.cpp
+++ Sorting_Algo.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e5 + 10;
-int a[N];
 
 void bubbleSort(int arr[], int n) {
     int i, j;
@@ -31,36 +29,30 @@ void selectionSort(int arr[], int n) {
         if(min_idx != i)swap(arr[min_idx], arr[i]);
     }
 }
-void merge(int l, int r, int mid) {
+void merge(int arr[], int l, int r, int mid) {
     int l_sz = mid - l + 1;
     int r_sz = r - (mid + 1) + 1;
     int L[l_sz+1], R[r_sz+1];
-    for(int i = 0; i < l_sz; i++) {
-        L[i] = a[i+l];
-    }
-    for(int i = 0; i < r_sz; i++) {
-        R[i] = a[i+mid+1];
-    }
+    for(int i = 0; i < l_sz; i++)L[i] = arr[i+l];
+    for(int i = 0; i < r_sz; i++)R[i] = arr[i+mid+1];
     L[l_sz] = R[r_sz] = INT_MAX;
-
     int l_i = 0, r_i = 0;
     for(int i = l; i <= r; i++) {
         if(L[l_i] <= R[r_i]) {
-            a[i] = L[l_i];
+            arr[i] = L[l_i];
             l_i++;
         }else {
-            a[i] = R[r_i];
+            arr[i] = R[r_i];
             r_i++;
         }
     }
-    return;
 }
-void mergeSort(int l, int r) {
+void mergeSort(int arr[], int l, int r) {
     if(l == r)return;
     int mid = l + (r - l)/2;
-    mergeSort(l, mid);
-    mergeSort(mid+1, r);
-    merge(l, r, mid);
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid+1, r);
+    merge(arr, l, r, mid);
 }
 int partition(int arr[], int low, int high) {
     int pivot = arr[high];
@@ -82,11 +74,11 @@ void quickSort(int arr[], int low, int high) {
     }
 }
 int main() {
-	int n;
+    int n;
     scanf("%d", &n);
     int arr[n];
-    for(int i = 0; i < n; i++)scanf("%d", &a[i]); 
-	mergeSort(0, n-1);
-	for(int i = 0; i < n; i++)printf("%d ", a[i]);
+    for(int i = 0; i < n; i++)scanf("%d", &arr[i]); 
+    quickSort(arr, 0, n-1);
+    for(int i = 0; i < n; i++)printf("%d ", arr[i]);
     return 0;
 }
