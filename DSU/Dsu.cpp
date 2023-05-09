d83b3ec33a46a56df7eba378cb28d7130fc66ddf
Emon <schrodingersrats@gmail.com>
2023-05-10 01:42:00 +0600

    Update Dsu.cpp

--- DSU/Dsu.cpp
+++ DSU/Dsu.cpp
@@ -1,26 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-int parent[1000+10];
+const int N = 1000;
+int parent[N], size[N], rank[N];
 
 void make_set(int v) {
     parent[v] = v;
-    return;
+    size[v] = 1;
+    //rank[v] = 0;
 }
 int find_set(int v) {
-    if (v == parent[v])
-        return v;
+    if (v == parent[v])return v;
     return parent[v] = find_set(parent[v]);
 }
 void union_sets(int a, int b) {
     a = find_set(a);
     b = find_set(b);
-    if (a != b)
+    if (a != b) {
+        if(size[a] < size[b])swap(a, b);
         parent[b] = a;
-    return;
+        size[a]+=size[b];
+    }
 }
+// Union by Rank
+// void union_sets(int a, int b) {
+//     a = find_set(a);
+//     b = find_set(b);
+//     if (a != b) {
+//         if (rank[a] < rank[b])
+//             swap(a, b);
+//         parent[b] = a;
+//         if (rank[a] == rank[b])
+//             rank[a]++;
+//     }
+// }
 int main() {
     for(int i = 1; i <= 10; i++) make_set(i);
     union_sets(1, 8);
     cout << parent[1] << " " << parent[8] << '\n';
-	return 0;
+    return 0;
 }
